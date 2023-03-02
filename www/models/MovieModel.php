c678ef24c9556f9be36da633586a1ec89853af99
Phucdanghoc <lep61748@gmail.com>
2023-03-03 00:05:17 +0700

    feat: movie CRUD start

--- www/models/MovieModel.php
+++ www/models/MovieModel.php
@@ -2,4 +2,35 @@
 class MovieModel
 {
     public $db;
+    public function getAll()
+    {
+        $sql = "Select * From MOVIE";
+        $stmt = $this->db->prepare($sql)->execute();
+        return $stmt;
+    }
+    public function getMovie($ID)
+    {
+        $sql = "Select * From MOVIE Where ID = '{$ID}'";
+        $stmt = $this->db->prepare($sql)->execute();
+        return $stmt;
+    }
+    public function getMoviebyDate()
+    {   $now =  date("Y-m-d");
+        $sql = "Select * From MOVIE Where DATE = '{$now}'";
+        $stmt = $this->db->prepare($sql)->execute();
+        return $stmt;
+    }
+    public function getMoviebyCatory($catory)
+    {
+        $sql = "Select * From MOVIE Where CATEGORY = '{$catory}'";
+        $stmt = $this->db->prepare($sql)->execute();
+        return $stmt;
+    }
+    public function FunctionName($rate)
+    {
+        $sql = "Select * From MOVIE Where RATE = '{$rate}'";
+        $stmt = $this->db->prepare($sql)->execute();
+        return $stmt;
+    }
 }
+?>
