e260b981e6ee1f0fbbb688c3d043b3d2b11c4845
DucCoops <99411358+DucCoops@users.noreply.github.com>
2023-03-21 00:27:35 +0700

    feat(foodcombo): CRUD API

---
+++ www/controllers/FoodComboController.php
@@ -0,0 +1,61 @@
+<?php
+class FoodComboController extends AdminController
+{ 
+    public $model;
+
+    function __construct()
+    {
+        $this->isAuthenticated();
+    }
+
+    public function getAll()
+    {
+        echo $this->model->getAll();
+    }
+
+    public function add()
+    {
+        if (
+            !isset($_POST['NAME'])
+            !isset($_POST['PRICE']) || !isset($_POST['TYPE']) || !isset($_POST['QUANTITY'])
+        ) {
+            die(json_encode(array('status' => false, 'data' => 'Parameters not valid')));
+        }
+
+        $NAME = $_POST['NAME'];
+        $PRICE = $_POST['PRICE'];
+        $TYPE = $_POST['TYPE'];
+        $QUANTITY = $_POST['QUANTITY'];
+        echo $this->model->add($NAME, $PRICE, $TYPE, $QUANTITY);
+    }
+
+    public function delete()
+    {
+        if (!isset($_POST['id'])) {
+            die(json_encode(array('status' => false, 'data' => 'Parameters not valid')));
+        }
+
+        $id = $_POST['id'];
+
+        echo $this->model->delete($id);
+    }
+
+
+    public function update()
+    {
+        if (
+            !isset($_POST['NAME'])
+            !isset($_POST['PRICE']) || !isset($_POST['TYPE']) || !isset($_POST['QUANTITY']) 
+            || !isset($_POST['ID'])
+        ) {
+            die(json_encode(array('status' => false, 'data' => 'Parameters not valid')));
+        }
+
+        $ID = $_POST['ID'];
+        $NAME = $_POST['NAME'];
+        $PRICE = $_POST['PRICE'];
+        $TYPE = $_POST['TYPE'];
+        $QUANTITY = $_POST['QUANTITY'];
+        echo $this->model->update($NAME, $PRICE, $TYPE, $QUANTITY, $ID);
+    }
+}
---
+++ www/models/FoodComboModel.php
@@ -0,0 +1,80 @@
+<?php
+
+class FoodComboModel
+{
+    public $db;
+
+    public function getAll()
+    {
+        $sql = 'SELECT * FROM foodcombo';
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+        } catch (PDOException $ex) {
+            die(json_encode(array('status' => false, 'data' => $ex->getMessage())));
+        }
+        $data = array();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $data[] = $row;
+        }
+
+        return json_encode(array('status' => true, 'data' => $data));
+    }
+
+    public function add($NAME, $PRICE, $TYPE, $QUANTITY)
+    {
+        $sql = 'INSERT INTO foodcombo(NAME, PRICE, TYPE, QUANTITY) VALUES(?,?,?,?)';
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute(array($NAME, $PRICE, $TYPE, $QUANTITY));
+
+            return json_encode(array('status' => true, 'data' => 'Thêm foodcombo thành công'));
+        } catch (PDOException $ex) {
+            die(json_encode(array('status' => false, 'data' => $ex->getMessage())));
+        }
+    }
+
+    public function delete($id)
+    {
+
+        $sql = 'DELETE FROM foodcombo where id = ?';
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute(array($id));
+
+            $count = $stmt->rowCount();
+
+            if ($count == 1) {
+                echo json_encode(array('status' => true, 'data' => 'Xóa sinh viên thành công'));
+            } else {
+                die(json_encode(array('status' => false, 'data' => 'Mã sinh viên không hợp lệ')));
+            }
+        } catch (PDOException $ex) {
+            die(json_encode(array('status' => false, 'data' => $ex->getMessage())));
+        }
+    }
+
+    public function update($NAME, $PRICE, $TYPE, $QUANTITY, $ID)
+    {
+
+
+        $sql = 'UPDATE `foodcombo` SET  `NAME` = ?,
+                `PRICE` = ?, `TYPE` = ?, `QUANTITY` = ? WHERE `foodcombo`.`ID` = ?';
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute(array($NAME, $PRICE, $TYPE, $QUANTITY, $ID));
+            $count = $stmt->rowCount();
+
+            if ($count == 1) {
+                echo json_encode(array('status' => true, 'data' => 'Cập nhật foodcombo thành công'));
+            } else {
+                die(json_encode(array('status' => false, 'data' => 'Không có foodcombo nào được cập nhật')));
+            }
+        } catch (PDOException $ex) {
+            die(json_encode(array('status' => false, 'data' => $ex->getMessage())));
+        }
+    }
+}
