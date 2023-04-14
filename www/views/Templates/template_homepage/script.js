46e18a66518d37b84704d14d231f5cee9d6ff4db
Le Tran Phu <52100572@student.tdtu.edu.vn>
2023-04-14 11:28:29 +0700

    style(homepage): add swiper

--- www/views/Templates/template_homepage/script.js
+++ www/views/Templates/template_homepage/script.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+    $swiper = new Swiper("#toppartSwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }
+    });
     $('#toggler-ongoing-movies').click(function (e) {
         $('#content-ongoing-movies').removeClass('d-none');
         $('#content-upcoming-movies').addClass('d-none');
