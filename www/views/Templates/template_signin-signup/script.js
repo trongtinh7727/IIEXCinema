1ac93eb892b4aaa127f1385031f42cde2904f8e3
Le Tran Phu <52100572@student.tdtu.edu.vn>
2023-04-20 23:16:49 +0700

    style(singin_signup): change

--- www/views/Templates/template_signin-signup/script.js
+++ www/views/Templates/template_signin-signup/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#tab-1').click(function (e) { 
         $('.login-wrap').css({
-            "height": "480px",
+            "height": "540px",
             "transition": ".3s"
         });
         $('#tab-2-label').removeClass('text-yellow')
@@ -9,7 +9,7 @@ $(document).ready(function() {
     });
     $('#tab-2').click(function (e) { 
         $('.login-wrap').css({
-            "height": "740px",
+            "height": "720px",
             "transition": ".3s"
         });
         $('#tab-1-label').removeClass('text-yellow')
